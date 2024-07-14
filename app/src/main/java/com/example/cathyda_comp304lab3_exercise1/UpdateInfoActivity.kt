3a7f2a1c185ebabf00e530e024c2b2174d916f0a
CDA <cathyda97@gmail.com>
2024-07-13 20:31:38 -0400

    skeleton of expected number of activities

---
+++ app/src/main/java/com/example/cathyda_comp304lab3_exercise1/UpdateInfoActivity.kt
@@ -0,0 +1,10 @@
+package com.example.cathyda_comp304lab3_exercise1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class UpdateInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
