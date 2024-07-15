a63c0ce47529061ae5b1d6cd5d6e6a20f112cca9
CDA <cathyda97@gmail.com>
2024-07-15 19:32:39 -0400

    created viewmodel for db

---
+++ app/src/main/java/com/example/cathyda_comp304lab3_exercise1/viewmodels/HospitalViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.cathyda_comp304lab3_exercise1.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.cathyda_comp304lab3_exercise1.database.hospital.HospitalDao
+import com.example.cathyda_comp304lab3_exercise1.database.hospital.NurseListModel
+import com.example.cathyda_comp304lab3_exercise1.database.hospital.PatientListModel
+import com.example.cathyda_comp304lab3_exercise1.database.hospital.TestListModel
+import kotlinx.coroutines.flow.Flow
+
+class HospitalViewModel(private val hospitalDao: HospitalDao): ViewModel() {
+
+    fun patientInfo(patientId: Int): Flow<PatientListModel> = hospitalDao.getPatientInfo(patientId);
+
+    fun testInfo(testId: Int): Flow<TestListModel> = hospitalDao.getTestInfo(testId)
+
+    fun patientNames(nurseId: Int): Flow<List<PatientListModel>> = hospitalDao.getPatientName(nurseId)
+
+    fun checkNursePassword(nurseId: Int): Flow<NurseListModel> = hospitalDao.getNursePassword(nurseId)
+}
+
+class HospitalViewModelFactory(
+    private val hospitalDao: HospitalDao
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T{
+        if(modelClass.isAssignableFrom(HospitalViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return HospitalViewModel(hospitalDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
