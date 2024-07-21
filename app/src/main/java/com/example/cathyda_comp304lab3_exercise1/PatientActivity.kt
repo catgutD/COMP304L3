3c408cb68e554d2559ed0fcd0cc8b2131b1fed51
CDA <cathyda97@gmail.com>
2024-07-21 17:09:58 -0400

    connected activities to layouts, updated login layout, created menu options, and created test layout for recycle view

--- app/src/main/java/com/example/cathyda_comp304lab3_exercise1/PatientActivity.kt
+++ app/src/main/java/com/example/cathyda_comp304lab3_exercise1/PatientActivity.kt
@@ -1,10 +1,19 @@
 package com.example.cathyda_comp304lab3_exercise1
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 
 class PatientActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_patient)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_options, menu)
+        return true
     }
 }
