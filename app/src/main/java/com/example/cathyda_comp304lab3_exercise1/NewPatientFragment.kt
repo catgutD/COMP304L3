318db4b39ddc3d2665315c0de0f24da3f0adcd24
CDA <cathyda97@gmail.com>
2024-07-24 23:20:21 -0400

    made new patient fragment dynamic in patient activity

--- app/src/main/java/com/example/cathyda_comp304lab3_exercise1/NewPatientFragment.kt
+++ app/src/main/java/com/example/cathyda_comp304lab3_exercise1/NewPatientFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.findFragment
 
 class NewPatientFragment : Fragment() {
     override fun onCreateView(
@@ -14,6 +16,17 @@ class NewPatientFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_new_patient, container, false)
         return view
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val btnAdd: Button? = view?.findViewById(R.id.btnAdd)
+        btnAdd?.setOnClickListener {
+            requireActivity().findViewById<View>(R.id.frgNewPatient).visibility = View.INVISIBLE
+            requireActivity().findViewById<View>(R.id.btnNewPatient).visibility = View.VISIBLE
+            requireActivity().findViewById<View>(R.id.btnViewPatient).visibility = View.VISIBLE
+        }
     }
 
 }
--- app/src/main/java/com/example/cathyda_comp304lab3_exercise1/PatientActivity.kt
+++ app/src/main/java/com/example/cathyda_comp304lab3_exercise1/PatientActivity.kt
@@ -12,10 +12,21 @@ class PatientActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_patient)
 
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frgNewPatient, NewPatientFragment())
+            .commit()
+
         val btnViewPatient: Button = findViewById<View>(R.id.btnViewPatient) as Button
         btnViewPatient.setOnClickListener {
             val intent = Intent(this, UpdateInfoActivity::class.java)
             startActivity(intent)
+        }
+
+        val btnNewPatient: Button = findViewById<View>(R.id.btnNewPatient) as Button
+        btnNewPatient.setOnClickListener {
+            btnNewPatient.visibility = View.INVISIBLE
+            btnViewPatient.visibility = View.INVISIBLE
+            findViewById<View>(R.id.frgNewPatient).visibility = View.VISIBLE
         }
     }
 
